21acf22cd0d144a697e0f83257bfe856997a52c5
Shikhar Uttma <uttam16shikhar@gmail.com>
2025-02-06 23:13:12 +0530

    cb is added

--- 6-callbacks/cb-hell.js
+++ 6-callbacks/cb-hell.js
@@ -1,3 +1,19 @@
-const fs = require('./index')
+const fs = require('fs')
 
-console.log(fs);
+fs.readFile('input.txt','utf-8',(err,data)=>{
+    if(err){
+        throw err
+    }
+    console.log('Data from file',data);
+    fs.writeFile('output.txt','This is output file',(err)=>{
+        if(err){
+            console.log('Error while writing file',err);
+        }
+        fs.readFile('output.txt','utf-8',(err,data)=>{
+            if(err){
+                console.log("Error while reading file",err)
+            }
+            console.log("Output file data",data)
+        })
+    })
+})
